2a13934dde31585b729d24d510e0d0691bdd4459
anirudh <anirudh04mishra@gmail.com>
2023-01-14 16:53:35 +0530

    fix: https

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -30,7 +30,7 @@ export default function Landing() {
   const [accountAddress, setAccountAddress] = useState("")
   const isConnectedToPeraWallet = !!accountAddress;
 
-  const algod = new Algodv2("2f3203f21e738a1de6110eba6984f9d03e5a95d7a577b34616854064cf2c0e7b", "http://node.testnet.algoexplorerapi.io","");
+  const algod = new Algodv2("2f3203f21e738a1de6110eba6984f9d03e5a95d7a577b34616854064cf2c0e7b", "https://node.testnet.algoexplorerapi.io","");
   async function generateAssetTransferTxns({
     to,
     assetID,
